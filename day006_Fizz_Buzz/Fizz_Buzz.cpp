db108380bb537b04cf01317bc0c5bfbfbf71b7bd
Jacob Cisneros <jakecx@proton.me>
2026-05-03 19:05:09 -0700

    Created Fizz_Buzz Program

---
+++ day006_Fizz_Buzz/Fizz_Buzz.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+int main() {
+
+	for (int i = 1; i <= 100; i++) {
+		if (i % 5 == 0 && i % 3 == 0) {
+			std::cout << "FizzBuzz\n";
+		}
+		else if (i % 5 == 0) {
+			std::cout << "Buzz\n";
+		}
+		else if ((i % 3) == 0) {
+			std::cout << "Fizz\n";
+		}
+		else {
+			std::cout << i << '\n';
+		}
+	}
+
+	std::cin.ignore();
+	std::cin.get();
+	return 0;
+}
